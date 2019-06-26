1b5b4a2ebfa58059b42e36c8e88a954ed48f045e
Ashutosh Panda <iamashutoshpanda@gmail.com>
2019-06-27 02:33:13 +0530

    Added Navbar Component 	-logo 	-input 	-ul
    Signed-off-by: Ashutosh Panda <iamashutoshpanda@gmail.com>

---
+++ task-codevector/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
+import './Toolbar.css';
+
+const toolbar = props => (
+  <header className="toolbar">
+    <nav className="toolbar__navigation">
+        <div className="toolbar__toggle-button">
+            <DrawerToggleButton click={props.drawerClickHandler} />
+        </div>
+        <div className="toolbar__logo"><a href="/">vPLAY</a></div>
+        <input type="text" className="search-input" placeholder="Search Now !"/>
+        <div className="spacer" />
+        <div className="toolbar_navigation-items">
+            <ul>
+                <li><a href="/">Networks</a></li>
+                <li><a href="/">Countries</a></li>
+            </ul>
+        </div>
+    </nav>
+  </header>
+);
+
+export default toolbar;
